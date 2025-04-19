7ec9445c7f5bf797b6a8dbacc9a5037e96879c35
Denys619 <denddeish1337@gmail.com>
2025-04-19 13:43:46 +0300

    Create DronePawn.h/cpp

---
+++ Source/DroneGame/DronePawn.cpp
@@ -0,0 +1,75 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DronePawn.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/FloatingPawnMovement.h"
+#include "Components/InputComponent.h"
+
+// Sets default values
+ADronePawn::ADronePawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	CameraComponent->SetupAttachment(RootComponent);
+
+	MovementComponent = CreateDefaultSubobject<UFloatingPawnMovement>(TEXT("MovementComponent"));
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+}
+
+// Called when the game starts or when spawned
+void ADronePawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADronePawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ADronePawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAxis("MoveForward", this, &ADronePawn::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ADronePawn::MoveRight);
+	PlayerInputComponent->BindAxis("MoveUp", this, &ADronePawn::MoveUp);
+
+	PlayerInputComponent->BindAxis("Turn", this, &ADronePawn::Turn);
+	PlayerInputComponent->BindAxis("LookUp", this, &ADronePawn::LookUp);
+}
+
+void ADronePawn::MoveForward(float Value)
+{
+	AddMovementInput(GetActorForwardVector(), Value);
+}
+
+void ADronePawn::MoveRight(float Value)
+{
+	AddMovementInput(GetActorRightVector(), Value);
+}
+
+void ADronePawn::MoveUp(float Value)
+{
+	AddMovementInput(GetActorUpVector(), Value);
+}
+
+void ADronePawn::Turn(float Value)
+{
+	AddControllerYawInput(Value);
+}
+
+void ADronePawn::LookUp(float Value)
+{
+	AddControllerPitchInput(Value);
+}
---
+++ Source/DroneGame/DronePawn.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "DronePawn.generated.h"
+
+UCLASS()
+class DRONEGAME_API ADronePawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ADronePawn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	class UCameraComponent* CameraComponent;
+
+	UPROPERTY(VisibleAnywhere)
+	class UFloatingPawnMovement* MovementComponent;
+
+	void MoveForward(float value);
+	void MoveRight(float value);
+	void MoveUp(float value);
+	void Turn(float value);
+	void LookUp(float value);
+};
